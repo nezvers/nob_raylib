e361b7a96d6e5b961a250a57d6b1d5dddb9c2c3d
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2025-11-20 23:52:42 +0200

    nob: build config & save previous build config

--- README.md
+++ README.md
@@ -19,15 +19,17 @@ Only requirements are:
 ## Features
 - All C build system. Easily implement project scaling.
 - "Passive" hot-reload with [adjust.h](https://github.com/bi3mer/adjust.h)
+- Supports platforms: Windows, Linux (X11). More to come.
 
 ## Compilation hints
 - On Linux if you get Raylib compilation error for `X11` you need to install dependencies recommended by [GLFW](https://www.glfw.org/docs/latest/compile.html)
 
 ## TODO
-- Compile only updated source files
 - Add WASM builds with emscriptem
+- Add Linux Wayland support
+- Compile only updated source files
 - Add option to fetch source files from provided directories
-- Rework and expand nob arguments (-help, -platform, -debug, -name)
+- Expand nob arguments (-help, -platform, -debug, -name)
 - Add compilation linking for APPLE (not personally interested, pull requests are welcome)  
 
 ## Supported IDE
--- nob.c
+++ nob.c
@@ -11,8 +11,10 @@
 #define INCLUDE_FOLDER "include/"
 #define BUILD_FOLDER "build/"
 #define BIN_FOLDER BUILD_FOLDER "bin/"
+#define WEB_FOLDER "web_build"
 #define DOWNLOAD_FOLDER "download/"
 #define DEPENDENCY_FOLDER "dependencies/"
+#define CONFIG_FILE_NAME ".config"
 
 #define RAYLIB_TAG "5.5"
 #define RAYLIB_TAR_FILE "raylib.tar.gz"
@@ -23,6 +25,31 @@
 
 static bool is_debug;
 static const char *project_name = PROJECT_NAME;
+
+enum PLATFORM_TARGET {
+	PLATFORM_DESKTOP,
+	PLATFORM_DESKTOP_GLFW,
+	PLATFORM_DESKTOP_SDL,
+	PLATFORM_DESKTOP_RGFW,
+	PLATFORM_WEB,
+	PLATFORM_DRM,
+	PLATFORM_ANDROID,
+};
+
+// For tracking last build settings that needs to be rebuild
+static int config_version = 1;
+struct SavedConfig {
+	enum PLATFORM_TARGET target_platform;
+	bool enable_wayland;
+};
+
+// It is set by build input arguments
+static struct SavedConfig current_config = {
+	PLATFORM_DESKTOP,
+	false,
+};
+// If successfully loaded config it will point to the data
+static struct SavedConfig *previous_config = NULL;
 
 // REDEFINE nob_cc - https://web.archive.org/web/20160308010351/https://beefchunk.com/documentation/lang/c/pre-defined-c/precomp.html
 #undef nob_cc
@@ -50,6 +77,66 @@ void nob_make(Nob_Cmd *cmd){
 #endif
 }
 
+int save_binary(const void *buffer, size_t size, const char *file_path, int bin_version){
+	FILE *file = fopen(file_path, "wb");
+    if (!file) {
+        nob_log(NOB_ERROR, "[ERROR] Open file to save: %s ", file_path);
+    	fclose(file);
+        return 1;
+    }
+
+	if (fwrite(&bin_version, sizeof(bin_version), 1, file) == 0){
+        nob_log(NOB_ERROR, "[ERROR] Couldn't save file bin version: %s ", file_path);
+    	fclose(file);
+		return 1;
+	}
+
+	if (fwrite(buffer, size, 1, file) == 0){
+        nob_log(NOB_ERROR, "[ERROR] Couldn't save file: %s ", file_path);
+   		fclose(file);
+		return 1;
+	}
+    fclose(file);
+
+	nob_log(NOB_INFO, "[INFO] Successful save file: %s ", file_path);
+	return 0;
+}
+
+int load_binary(void *buffer, size_t size, const char *file_path, int bin_version){
+	if (!nob_file_exists(file_path)){
+		nob_log(NOB_INFO, "[INFO] File to load doesn't exist: %s", file_path);
+		return 1;
+	}
+	FILE *file = fopen(file_path, "rb");
+    if (!file) {
+        nob_log(NOB_ERROR, "[ERROR] Open file to load: %s ", file_path);
+        return 1;
+    }
+	
+	int saved_version;
+	if (fread(&saved_version, sizeof(saved_version), 1, file) == 0){
+		nob_log(NOB_ERROR, "[ERROR] Couldn't load file bin version: %s ", file_path);
+    	fclose(file);
+        return 1;
+	}
+
+	if (saved_version != bin_version){
+		nob_log(NOB_INFO, "[INFO] File bin version missmatch (skip loading): %s (%d != %d)", file_path, saved_version, bin_version);
+    	fclose(file);
+        return 1;
+	}
+
+	if (fread(buffer, size, 1, file) == 0){
+		nob_log(NOB_ERROR, "[ERROR] Couldn't load file: %s ", file_path);
+    	fclose(file);
+        return 1;
+	}
+    fclose(file);
+
+	nob_log(NOB_INFO, "[INFO] Successful load file: %s ", file_path);
+	return 0;
+}
+
 // Downloads file using curl or fallbacks to wget
 void download_file(const char *url, const char *dest) {
 	char download_cmd[2048] = {0};
@@ -57,7 +144,7 @@ void download_file(const char *url, const char *dest) {
 	if (system(download_cmd) != 0) {
 		snprintf(download_cmd, sizeof(download_cmd), "wget -q \"%s\" -O \"%s\"", url, dest);
 		if (system(download_cmd) != 0) {
-			fprintf(stderr, "Failed to download %s\n", url);
+			nob_log(NOB_ERROR, "Failed to download %s\n", url);
 		}
 	}
 }
@@ -71,6 +158,18 @@ int extract_tar_archive(const char *archive_path, const char *target_dir){
 		return 1;
 	}
 	return 0;
+}
+
+const char *get_raylib_platform(enum PLATFORM_TARGET platform){
+	switch (platform){
+		case PLATFORM_DESKTOP_GLFW: return "PLATFORM=PLATFORM_DESKTOP_GLFW";
+		case PLATFORM_DESKTOP_SDL: return "PLATFORM=PLATFORM_DESKTOP_SDL";
+		case PLATFORM_DESKTOP_RGFW: return "PLATFORM=PLATFORM_DESKTOP_RGFW";
+		case PLATFORM_WEB: return "PLATFORM=PLATFORM_WEB";
+		case PLATFORM_DRM: return "PLATFORM=PLATFORM_DRM";
+		case PLATFORM_ANDROID: return "PLATFORM=PLATFORM_ANDROID";
+		default: return "PLATFORM=PLATFORM_DESKTOP";
+	}
 }
 
 int setup_raylib(Nob_Cmd *cmd){
@@ -94,20 +193,31 @@ int setup_raylib(Nob_Cmd *cmd){
 	}
 
 	// Compile
-	if (!nob_file_exists(RAYLIB_SRC_DIR "libraylib.a")){
-		nob_log(NOB_INFO, "[INFO] Changing directory: %s ", RAYLIB_SRC_DIR);
-		if (!nob_set_current_dir(nob_temp_sprintf("./%s", RAYLIB_SRC_DIR))) return 1;
+	bool need_rebuild = previous_config != NULL && previous_config->target_platform != current_config.target_platform;
+	if (!nob_file_exists(RAYLIB_SRC_DIR "libraylib.a") || need_rebuild){
+		nob_log(NOB_INFO, "Changing directory: %s ", RAYLIB_SRC_DIR);
+		if (!nob_set_current_dir(nob_temp_sprintf("./%s", RAYLIB_SRC_DIR))){
+			nob_log(NOB_ERROR, "Failed to change to directory: %s", RAYLIB_SRC_DIR);
+			return 1;
+		}
 
 		nob_make(cmd);
-		nob_cmd_append(cmd, "PLATFORM=" RAYLIB_PLATFORM);
-		if (!nob_cmd_run(cmd)) return 1;
+		const char *raylib_platform = get_raylib_platform(current_config.target_platform);
+		nob_cmd_append(cmd, raylib_platform);
+		if (!nob_cmd_run(cmd)){
+			nob_log(NOB_ERROR, "Failed to compile raylib");
+			return 1;
+		}
 
-		nob_log(NOB_INFO, "[INFO] Changing directory: %s ", "../../../");
-		if (!nob_set_current_dir("../../../")) return 1;
+		nob_log(NOB_INFO, "Changing directory: %s ", "../../../");
+		if (!nob_set_current_dir("../../../")){
+			nob_log(NOB_ERROR, "Failed to move bact to root directory");
+			return 1;
+		}
 	}
 
 	if (!nob_file_exists(RAYLIB_SRC_DIR "libraylib.a")){
-		nob_log(NOB_ERROR, "[ERROR] Failed to compile raylib");
+		nob_log(NOB_ERROR, "libraylib.a disappeared!!!");
 		return 1;
 	}
 
@@ -121,11 +231,17 @@ void get_include_raylib(Nob_Cmd *cmd){
 void link_raylib(Nob_Cmd *cmd){
 	nob_cmd_append(cmd, "-L", RAYLIB_SRC_DIR, "-lraylib");
 
-#if defined(WINDOWS)
-	nob_cmd_append(cmd, "-lgdi32", "-lwinmm", "-lopengl32");
-#elif defined(LINUX)
-	nob_cmd_append(cmd, "-lm", "-ldl", "-lpthread", "-lGL", "-lX11");
-#endif
+	switch (current_config.target_platform){
+		case (PLATFORM_DESKTOP):
+		case (PLATFORM_DESKTOP_GLFW):
+		case (PLATFORM_DESKTOP_RGFW):
+	#if defined(WINDOWS)
+			nob_cmd_append(cmd, "-lgdi32", "-lwinmm", "-lopengl32");
+	#elif defined(LINUX)
+			nob_cmd_append(cmd, "-lm", "-ldl", "-lpthread", "-lGL", "-lX11");
+	#endif
+			break;
+	}
 }
 
 void get_include_directories(Nob_Cmd *cmd){
@@ -133,17 +249,34 @@ void get_include_directories(Nob_Cmd *cmd){
 }
 
 void get_defines(Nob_Cmd *cmd){
-
-	// RESOURCES_PATH macro definition
 	if (is_debug){
-		nob_cmd_append(cmd, "-D", nob_temp_sprintf("RESOURCES_PATH=%s", "../resources/"));
+		// Debug symbols
+		nob_cmd_append(cmd, "-g");
+		nob_cmd_append(cmd, "-O0", "-DDEBUG");
+		switch (current_config.target_platform){
+			case (PLATFORM_DESKTOP):
+			case (PLATFORM_DESKTOP_GLFW):
+			case (PLATFORM_DESKTOP_RGFW):
+				nob_cmd_append(cmd, "-D", nob_temp_sprintf("RESOURCES_PATH=%s", "../resources/"));
+				break;
+			default:
+				nob_cmd_append(cmd, "-D", nob_temp_sprintf("RESOURCES_PATH=%s", "../resources/"));	
+		}
 	}
 	else{
 		nob_cmd_append(cmd, "-D", nob_temp_sprintf("RESOURCES_PATH=%s", "resources/"));
 	}
 
-	// Debug symbols
-	if (is_debug) nob_cmd_append(cmd, "-g");
+	if (current_config.target_platform == PLATFORM_WEB){
+		nob_cmd_append(cmd, "-Os", "-Wall");
+		nob_cmd_append(cmd, "-s", "USE_GLFW=3");
+		nob_cmd_append(cmd, "-s", "ASSERTIONS=1");
+		nob_cmd_append(cmd, "-s", "WASM=1");
+		nob_cmd_append(cmd, "-s", "TOTAL_MEMORY=67108864");
+		nob_cmd_append(cmd, "-s", "FORCE_FILESYSTEM=1");
+		nob_cmd_append(cmd, "--preload-file", "resources/");
+		nob_cmd_append(cmd, "--shell-file", "../minshell.html");
+	}
 }
 
 int get_source_files(Nob_Cmd *cmd){
@@ -180,6 +313,13 @@ int get_source_files(Nob_Cmd *cmd){
 	return 0;
 }
 
+int setup_web(Nob_Cmd *cmd){
+	if (!nob_mkdir_if_not_exists(WEB_FOLDER)) return 1;
+	if (!nob_mkdir_if_not_exists(WEB_FOLDER)) return 1;
+
+	return 0;
+}
+
 int main(int argc, char **argv){
 	NOB_GO_REBUILD_URSELF(argc, argv);
 
@@ -203,8 +343,17 @@ int main(int argc, char **argv){
 	if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
 	if (!nob_mkdir_if_not_exists(BIN_FOLDER)) return 1;
 
+	struct SavedConfig saved_config = {0};
+	if (load_binary(&saved_config, sizeof(saved_config), BUILD_FOLDER CONFIG_FILE_NAME, config_version) == 0){
+		previous_config = &saved_config;
+	}
+
 	Nob_Cmd cmd = {0};
 	if (setup_raylib(&cmd) != 0){
+		return 1;
+	}
+
+	if (current_config.target_platform == PLATFORM_WEB && setup_web(&cmd) != 0){
 		return 1;
 	}
 
@@ -222,6 +371,10 @@ int main(int argc, char **argv){
 	link_raylib(&cmd);
 
 	if (!nob_cmd_run(&cmd)) return 1;
+
+	if (save_binary(&current_config, sizeof(current_config), BUILD_FOLDER CONFIG_FILE_NAME, config_version) != 0){
+		return 1;
+	}
 
 	return 0;
 }
