ee83734f669ca6a51bd46411498c85079d17439a
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2025-11-22 12:19:07 +0200

    nob: refactor with RESULT, separate nob_utils.h

---
+++ include/nob_utils.h
@@ -0,0 +1,184 @@
+#ifndef NOB_UTILS_H
+#define NOB_UTILS_H
+
+// don't include nob.h, asume it is already included before this
+
+// REDEFINE nob_cc - https://web.archive.org/web/20160308010351/https://beefchunk.com/documentation/lang/c/pre-defined-c/precomp.html
+#undef nob_cc
+#if _WIN32
+#	define WINDOWS
+#	if defined(__MINGW32__)
+#		define nob_cc(cmd) nob_cmd_append(cmd, "gcc")
+#	elif defined(__GNUC__)
+#		define nob_cc(cmd) nob_cmd_append(cmd, "cc")
+#	elif defined(__clang__)
+#		define nob_cc(cmd) nob_cmd_append(cmd, "clang")
+#	elif defined(_MSC_VER)
+#		define nob_cc(cmd) nob_cmd_append(cmd, "cl.exe")
+#	endif
+#else
+#	define LINUX
+#	define nob_cc(cmd) nob_cmd_append(cmd, "cc")
+#endif
+
+enum RESULT {
+	SUCCESS,
+	FAILED,
+};
+
+enum PLATFORM_TARGET {
+	PLATFORM_DESKTOP,
+	PLATFORM_DESKTOP_GLFW,
+	PLATFORM_DESKTOP_SDL,
+	PLATFORM_DESKTOP_RGFW,
+	PLATFORM_WEB,
+	PLATFORM_DRM,
+	PLATFORM_ANDROID,
+};
+
+struct SavedConfig {
+	bool is_debug;
+	enum PLATFORM_TARGET platform;
+	bool enable_wayland;
+};
+
+void swap_dir_slashes(char *dir_path, int length){
+	for (int i = 0; i < length; ++i){
+		if (dir_path[i] == '\0') break;
+		if (dir_path[i] == '/') dir_path[i] = '\\';
+		else if (dir_path[i] == '\\') dir_path[i] = '/';
+	}
+}
+
+enum RESULT delete_directory(const char *dir_path){
+#if defined(WINDOWS)
+	char dir_buffer[1024] = {0};
+	snprintf(dir_buffer, sizeof(dir_buffer) / sizeof(dir_buffer[0]), "%s", dir_path);
+	swap_dir_slashes(dir_buffer, sizeof(dir_buffer) / sizeof(dir_buffer[0]));
+	if (system(nob_temp_sprintf("cmd.exe /c \"rmdir /s /q %s \"", dir_buffer)) == FAILED){
+		nob_log(NOB_ERROR, "Failed to delete a directory: %s", dir_path);
+		return FAILED;
+	}
+#elif defined(LINUX)
+	Nob_Cmd rm_cmd = {0};
+	nob_cmd_append(&rm_cmd, "rm", "-rf", dir_path, "||", "true");
+	if (!nob_cmd_run(&rm_cmd)){
+		nob_log(NOB_ERROR, "Failed to delete a directory: %s", dir_path);
+		return FAILED;
+	}
+#endif
+	return SUCCESS;
+}
+
+void nob_make(Nob_Cmd *cmd){
+#if defined(__MINGW32__)
+	nob_cmd_append(cmd, "mingw32-make");
+#else
+	nob_cmd_append(cmd, "make");
+#endif
+}
+
+enum RESULT save_binary(const void *buffer, size_t size, const char *file_path, int bin_version){
+	FILE *file = fopen(file_path, "wb");
+    if (!file) {
+        nob_log(NOB_ERROR, "Open file to save: %s ", file_path);
+    	fclose(file);
+        return FAILED;
+    }
+
+	if (fwrite(&bin_version, sizeof(bin_version), 1, file) == 0){
+        nob_log(NOB_ERROR, "Couldn't save file bin version: %s ", file_path);
+    	fclose(file);
+		return FAILED;
+	}
+
+	if (fwrite(buffer, size, 1, file) == 0){
+        nob_log(NOB_ERROR, "Couldn't save file: %s ", file_path);
+   		fclose(file);
+		return FAILED;
+	}
+    fclose(file);
+
+	nob_log(NOB_INFO, "Successful save file: %s ", file_path);
+	return SUCCESS;
+}
+
+enum RESULT load_binary(void *buffer, size_t size, const char *file_path, int bin_version){
+	if (!nob_file_exists(file_path)){
+		nob_log(NOB_INFO, "File to load doesn't exist: %s", file_path);
+		return FAILED;
+	}
+	FILE *file = fopen(file_path, "rb");
+    if (!file) {
+        nob_log(NOB_ERROR, "Open file to load: %s ", file_path);
+        return FAILED;
+    }
+	
+	int saved_version;
+	if (fread(&saved_version, sizeof(saved_version), 1, file) == 0){
+		nob_log(NOB_ERROR, "Couldn't load file bin version: %s ", file_path);
+    	fclose(file);
+        return FAILED;
+	}
+
+	if (saved_version != bin_version){
+		nob_log(NOB_INFO, "File bin version missmatch (skip loading): %s (%d != %d)", file_path, saved_version, bin_version);
+    	fclose(file);
+        return FAILED;
+	}
+
+	if (fread(buffer, size, 1, file) == 0){
+		nob_log(NOB_ERROR, "Couldn't load file: %s ", file_path);
+    	fclose(file);
+        return FAILED;
+	}
+    fclose(file);
+
+	nob_log(NOB_INFO, "Successful load file: %s ", file_path);
+	return SUCCESS;
+}
+
+// Downloads file using curl or fallbacks to wget
+enum RESULT download_file(const char *url, const char *dest) {
+	char download_cmd[2048] = {0};
+	snprintf(download_cmd, sizeof(download_cmd), "curl -fsSL \"%s\" -o \"%s\"", url, dest);
+	nob_log(NOB_INFO, "Downloading file: %s", dest);
+	if (system(download_cmd) == FAILED) {
+		snprintf(download_cmd, sizeof(download_cmd), "wget -q \"%s\" -O \"%s\"", url, dest);
+		if (system(download_cmd) == FAILED) {
+			nob_log(NOB_ERROR, "Failed to download %s\n", url);
+			return FAILED;
+		}
+	}
+	return SUCCESS;
+}
+
+enum RESULT extract_tar_archive(const char *archive_path, const char *target_dir, unsigned int strip_lvl){
+	char tar_cmd[2048] = {0};
+	snprintf(tar_cmd, sizeof(tar_cmd), "tar -xzf \"%s\" -C \"%s\" --strip-components=%d", archive_path, target_dir, strip_lvl);
+
+	if (system(tar_cmd)){
+		nob_log(NOB_ERROR, "Failed to extract: %s -> %s\nCMD: %s", archive_path, target_dir, tar_cmd);
+		return FAILED;
+	}
+	return SUCCESS;
+}
+
+enum RESULT extract_zip_archive(const char *archive_path, const char *target_dir){
+	char zip_cmd[2048] = {0};
+#if defined(WINDOWS)
+	// Slow as shit
+	// snprintf(zip_cmd, sizeof(zip_cmd), "powershell -command \"Expand-Archive -Force '%s' '%s'", archive_path, target_dir);
+	snprintf(zip_cmd, sizeof(zip_cmd), "tar -xf \"%s\" -C \"%s\"", archive_path, target_dir);
+#elif defined(LINUX)
+	snprintf(zip_cmd, sizeof(zip_cmd), "tar -xf \"%s\" -C \"%s\"", archive_path, target_dir);
+#endif
+
+	if (system(zip_cmd)){
+		nob_log(NOB_ERROR, "Failed to extract: %s -> %s\nCMD: %s", archive_path, target_dir, zip_cmd);
+		return FAILED;
+	}
+	return SUCCESS;
+}
+
+#endif //NOB_UTILS_H
--- nob.c
+++ nob.c
@@ -4,6 +4,7 @@
 #include "include/nob.h" // https://github.com/tsoding/nob.h
 #include <stdio.h>
 #include <string.h>
+#include "include/nob_utils.h"
 
 #define PROJECT_NAME "nob_raylib"
 
@@ -11,7 +12,7 @@
 #define INCLUDE_FOLDER "include/"
 #define BUILD_FOLDER "build/"
 #define BIN_FOLDER BUILD_FOLDER "bin/"
-#define WEB_FOLDER "web_build"
+#define WEB_FOLDER "web_build/"
 #define RESOURCES_FOLDER "resources/"
 #define DOWNLOAD_FOLDER "download/"
 #define DEPENDENCY_FOLDER "dependencies/"
@@ -24,143 +25,28 @@
 #define RAYLIB_ARCHIVE DOWNLOAD_FOLDER RAYLIB_TAR_FILE
 #define RAYLIB_SRC_DIR DEPENDENCY_FOLDER RAYLIB_DIR_NAME "src/"
 
-static bool is_debug;
-static const char *project_name = PROJECT_NAME;
+#define EMSCRIPTEN_TAG "4.0.20"
+#define EMSCRIPTEN_TAR_FILE "emscripten.tar.gz"
+#define EMSCRIPTEN_DIR_NAME "emscripten/"
+#define EMSCRIPTEN_ARCHIVE DOWNLOAD_FOLDER EMSCRIPTEN_TAR_FILE
+#define EMSCRIPTEN_SRC_DIR DEPENDENCY_FOLDER EMSCRIPTEN_DIR_NAME
 
-enum PLATFORM_TARGET {
-	PLATFORM_DESKTOP,
-	PLATFORM_DESKTOP_GLFW,
-	PLATFORM_DESKTOP_SDL,
-	PLATFORM_DESKTOP_RGFW,
-	PLATFORM_WEB,
-	PLATFORM_DRM,
-	PLATFORM_ANDROID,
-};
+static const char *project_name = PROJECT_NAME;
 
 // For tracking last build settings that needs to be rebuild
 static int config_version = 1;
-struct SavedConfig {
-	enum PLATFORM_TARGET platform;
-	bool enable_wayland;
-};
 
 // It is set by build input arguments
 static struct SavedConfig current_config = {
+	false,
 	PLATFORM_DESKTOP,
 	false,
 };
 // If successfully loaded config it will point to the data
 static struct SavedConfig *previous_config = NULL;
 
-// REDEFINE nob_cc - https://web.archive.org/web/20160308010351/https://beefchunk.com/documentation/lang/c/pre-defined-c/precomp.html
-#undef nob_cc
-#if _WIN32
-#	define WINDOWS
-#	if defined(__MINGW32__)
-#		define nob_cc(cmd) nob_cmd_append(cmd, "gcc")
-#	elif defined(__GNUC__)
-#		define nob_cc(cmd) nob_cmd_append(cmd, "cc")
-#	elif defined(__clang__)
-#		define nob_cc(cmd) nob_cmd_append(cmd, "clang")
-#	elif defined(_MSC_VER)
-#		define nob_cc(cmd) nob_cmd_append(cmd, "cl.exe")
-#	endif
-#else
-#	define LINUX
-#	define nob_cc(cmd) nob_cmd_append(cmd, "cc")
-#endif
-
-void nob_make(Nob_Cmd *cmd){
-#if defined(__MINGW32__)
-	nob_cmd_append(cmd, "mingw32-make");
-#else
-	nob_cmd_append(cmd, "make");
-#endif
-}
-
-int save_binary(const void *buffer, size_t size, const char *file_path, int bin_version){
-	FILE *file = fopen(file_path, "wb");
-    if (!file) {
-        nob_log(NOB_ERROR, "[ERROR] Open file to save: %s ", file_path);
-    	fclose(file);
-        return 1;
-    }
-
-	if (fwrite(&bin_version, sizeof(bin_version), 1, file) == 0){
-        nob_log(NOB_ERROR, "[ERROR] Couldn't save file bin version: %s ", file_path);
-    	fclose(file);
-		return 1;
-	}
-
-	if (fwrite(buffer, size, 1, file) == 0){
-        nob_log(NOB_ERROR, "[ERROR] Couldn't save file: %s ", file_path);
-   		fclose(file);
-		return 1;
-	}
-    fclose(file);
-
-	nob_log(NOB_INFO, "[INFO] Successful save file: %s ", file_path);
-	return 0;
-}
-
-int load_binary(void *buffer, size_t size, const char *file_path, int bin_version){
-	if (!nob_file_exists(file_path)){
-		nob_log(NOB_INFO, "[INFO] File to load doesn't exist: %s", file_path);
-		return 1;
-	}
-	FILE *file = fopen(file_path, "rb");
-    if (!file) {
-        nob_log(NOB_ERROR, "[ERROR] Open file to load: %s ", file_path);
-        return 1;
-    }
-	
-	int saved_version;
-	if (fread(&saved_version, sizeof(saved_version), 1, file) == 0){
-		nob_log(NOB_ERROR, "[ERROR] Couldn't load file bin version: %s ", file_path);
-    	fclose(file);
-        return 1;
-	}
-
-	if (saved_version != bin_version){
-		nob_log(NOB_INFO, "[INFO] File bin version missmatch (skip loading): %s (%d != %d)", file_path, saved_version, bin_version);
-    	fclose(file);
-        return 1;
-	}
-
-	if (fread(buffer, size, 1, file) == 0){
-		nob_log(NOB_ERROR, "[ERROR] Couldn't load file: %s ", file_path);
-    	fclose(file);
-        return 1;
-	}
-    fclose(file);
-
-	nob_log(NOB_INFO, "[INFO] Successful load file: %s ", file_path);
-	return 0;
-}
-
-// Downloads file using curl or fallbacks to wget
-void download_file(const char *url, const char *dest) {
-	char download_cmd[2048] = {0};
-	snprintf(download_cmd, sizeof(download_cmd), "curl -fsSL \"%s\" -o \"%s\"", url, dest);
-	if (system(download_cmd) != 0) {
-		snprintf(download_cmd, sizeof(download_cmd), "wget -q \"%s\" -O \"%s\"", url, dest);
-		if (system(download_cmd) != 0) {
-			nob_log(NOB_ERROR, "Failed to download %s\n", url);
-		}
-	}
-}
-
-int extract_tar_archive(const char *archive_path, const char *target_dir){
-	char tar_cmd[2048] = {0};
-	snprintf(tar_cmd, sizeof(tar_cmd), "tar -xzf \"%s\" -C \"%s\" --strip-components=1", archive_path, target_dir);
-
-	if (system(tar_cmd)){
-		nob_log(NOB_ERROR, "[ERROR] Failed to extract: %s -> %s\nCMD: %s", archive_path, target_dir, tar_cmd);
-		return 1;
-	}
-	return 0;
-}
-
+//---------------------------------------------------------------------------------
+// Raylib
 const char *get_raylib_platform(enum PLATFORM_TARGET platform){
 	switch (platform){
 		case PLATFORM_DESKTOP_GLFW: return "PLATFORM=PLATFORM_DESKTOP_GLFW";
@@ -173,23 +59,26 @@ const char *get_raylib_platform(enum PLATFORM_TARGET platform){
 	}
 }
 
-int setup_raylib(Nob_Cmd *cmd){
+enum RESULT setup_raylib(Nob_Cmd *cmd){
 	const char *raylib_url = "https://github.com/raysan5/raylib/archive/refs/tags/" RAYLIB_TAG ".tar.gz";
 
 	// Download
 	if (!nob_file_exists(RAYLIB_ARCHIVE)){
-		download_file(raylib_url, RAYLIB_ARCHIVE);
+		if (download_file(raylib_url, RAYLIB_ARCHIVE) == FAILED){
+			return FAILED;
+		}
 
 		if (!nob_file_exists(RAYLIB_ARCHIVE)){
-			return 1;
+			nob_log(NOB_ERROR, "Just downloaded file dissapeared");
+			return FAILED;
 		}
 	}
 
 	// Extract
-	if (!nob_mkdir_if_not_exists(DEPENDENCY_FOLDER RAYLIB_DIR_NAME)) return 1;
+	if (!nob_mkdir_if_not_exists(DEPENDENCY_FOLDER RAYLIB_DIR_NAME)) return FAILED;
 	if (!nob_file_exists(RAYLIB_SRC_DIR "raylib.h")){
-		if(extract_tar_archive(RAYLIB_ARCHIVE, DEPENDENCY_FOLDER RAYLIB_DIR_NAME)){
-			return 1;
+		if(extract_tar_archive(RAYLIB_ARCHIVE, DEPENDENCY_FOLDER RAYLIB_DIR_NAME, 1)){
+			return FAILED;
 		}
 	}
 
@@ -199,7 +88,7 @@ int setup_raylib(Nob_Cmd *cmd){
 		nob_log(NOB_INFO, "Changing directory: %s ", RAYLIB_SRC_DIR);
 		if (!nob_set_current_dir(nob_temp_sprintf("./%s", RAYLIB_SRC_DIR))){
 			nob_log(NOB_ERROR, "Failed to change to directory: %s", RAYLIB_SRC_DIR);
-			return 1;
+			return FAILED;
 		}
 
 		nob_make(cmd);
@@ -209,33 +98,33 @@ int setup_raylib(Nob_Cmd *cmd){
 			nob_cmd_append(&emsdk_cmd, "emsdk", "activate", "latest");
 			if (!nob_cmd_run(&emsdk_cmd)){
 				nob_log(NOB_ERROR, "Failed to activate latest emscriptem");
-				return 1;
+				return FAILED;
 			}
 			nob_cmd_append(&emsdk_cmd, "emsdk_env");
 			if (!nob_cmd_run(&emsdk_cmd)){
 				nob_log(NOB_ERROR, "Failed to initiate emscriptem environment");
-				return 1;
+				return FAILED;
 			}
 		}
 		nob_cmd_append(cmd, raylib_platform);
 		if (!nob_cmd_run(cmd)){
 			nob_log(NOB_ERROR, "Failed to compile raylib");
-			return 1;
+			return FAILED;
 		}
 
 		nob_log(NOB_INFO, "Changing directory: %s ", "../../../");
 		if (!nob_set_current_dir("../../../")){
 			nob_log(NOB_ERROR, "Failed to move bact to root directory");
-			return 1;
+			return FAILED;
 		}
 	}
 
 	if (!nob_file_exists(RAYLIB_SRC_DIR "libraylib.a")){
 		nob_log(NOB_ERROR, "libraylib.a disappeared!!!");
-		return 1;
+		return FAILED;
 	}
 
-	return 0;
+	return SUCCESS;
 }
 
 void get_include_raylib(Nob_Cmd *cmd){
@@ -257,13 +146,80 @@ void link_raylib(Nob_Cmd *cmd){
 			break;
 	}
 }
+//---------------------------------------------------------------------------------
+// Emscripten
+enum RESULT setup_emscripten(Nob_Cmd *cmd){
+	const char *emscripten_url = "https://github.com/emscripten-core/emscripten/archive/refs/tags/" EMSCRIPTEN_TAG ".tar.gz";
+	// EMSCRIPTEN_TAG
+	// EMSCRIPTEN_TAR_FILE
+	// EMSCRIPTEN_DIR_NAME
+	// EMSCRIPTEN_ARCHIVE
+	// EMSCRIPTEN_SRC_DIR
+	// Download
+	if (!nob_file_exists(EMSCRIPTEN_ARCHIVE)){
+		if (download_file(emscripten_url, EMSCRIPTEN_ARCHIVE) == FAILED){
+			return FAILED;
+		}
 
+		if (!nob_file_exists(EMSCRIPTEN_ARCHIVE)){
+			nob_log(NOB_ERROR, "Just downloaded file dissapeared");
+			return FAILED;
+		}
+	}
+
+	// Extract
+	if (!nob_mkdir_if_not_exists(DEPENDENCY_FOLDER EMSCRIPTEN_DIR_NAME)) return FAILED;
+	if (!nob_file_exists(EMSCRIPTEN_SRC_DIR "README.md")){
+		if(extract_tar_archive(EMSCRIPTEN_ARCHIVE, DEPENDENCY_FOLDER EMSCRIPTEN_DIR_NAME, 1)){
+			return FAILED;
+		}
+	}
+
+	if (!nob_file_exists(EMSCRIPTEN_SRC_DIR ".emscripten")){
+#if defined(WINDOWS)
+		system(EMSCRIPTEN_SRC_DIR ".emsdk.bat install latest");
+#elif defined(LINUX)
+		system(EMSCRIPTEN_SRC_DIR ".emsdk install latest");
+#endif
+	}
+
+#if defined(WINDOWS)
+	if (system(EMSCRIPTEN_SRC_DIR ".emsdk.bat activate latest") != 0){return FAILED;}
+	if (system(EMSCRIPTEN_SRC_DIR "emsdk_env.bat") != 0){return FAILED;}
+#elif defined(LINUX)
+	if (system(EMSCRIPTEN_SRC_DIR "emsdk activate latest") != 0){return FAILED;}
+	if (system("source " EMSCRIPTEN_SRC_DIR "emsdk_env.sh") != 0){return FAILED;}
+#endif
+
+	return SUCCESS;
+}
+
+enum RESULT setup_web(Nob_Cmd *cmd){
+	if (setup_emscripten(cmd) == FAILED){
+		return FAILED;
+	}
+
+	if (!nob_mkdir_if_not_exists(WEB_FOLDER)) return FAILED;
+	if (nob_file_exists(WEB_FOLDER RESOURCES_FOLDER)){
+		if (delete_directory(WEB_FOLDER RESOURCES_FOLDER) == FAILED){
+			return FAILED;
+		}
+	}
+	if (!nob_mkdir_if_not_exists(WEB_FOLDER RESOURCES_FOLDER)) return FAILED;
+	if (!nob_copy_directory_recursively(RESOURCES_FOLDER, WEB_FOLDER RESOURCES_FOLDER)){
+		return FAILED;
+	}
+
+	return SUCCESS;
+}
+//---------------------------------------------------------------------------------
+// Source
 void get_include_directories(Nob_Cmd *cmd){
 	nob_cmd_append(cmd, "-I", INCLUDE_FOLDER);
 }
 
 void get_defines(Nob_Cmd *cmd){
-	if (is_debug){
+	if (current_config.is_debug){
 		// Debug symbols
 		nob_cmd_append(cmd, "-g");
 		nob_cmd_append(cmd, "-O0", "-DDEBUG");
@@ -293,7 +249,7 @@ void get_defines(Nob_Cmd *cmd){
 	}
 }
 
-int get_source_files(Nob_Cmd *cmd){
+enum RESULT get_source_files(Nob_Cmd *cmd){
 	// Build source objects in parallel
 	Nob_Cmd obj_cmd = {0};
 	Nob_Procs procs = {0};
@@ -307,40 +263,30 @@ int get_source_files(Nob_Cmd *cmd){
 	// TODO: Add check if object files are changed
 	for (size_t i = 0; i < NOB_ARRAY_LEN(targets); ++i){
 		nob_cc(&obj_cmd);
-		nob_da_append(&obj_cmd, "-c");
-		nob_da_append(&obj_cmd, targets[i].src_path);
-		nob_da_append(&obj_cmd, "-o");
-		nob_da_append(&obj_cmd, targets[i].bin_path);
+		nob_cmd_append(&obj_cmd, "-c", targets[i].src_path);
+		nob_cmd_append(&obj_cmd, "-o", targets[i].bin_path);
 		nob_cc_flags(&obj_cmd);
 		get_defines(&obj_cmd);
 		get_include_raylib(&obj_cmd);
 		get_include_directories(&obj_cmd);
-		if (!nob_cmd_run(&obj_cmd, .async = &procs)) return 1;
+		if (!nob_cmd_run(&obj_cmd, .async = &procs)){
+			nob_log(NOB_ERROR, "Appending build to queue failed");
+			return FAILED;
+		}
 	}
 
 	// Wait on all the async processes to finish and reset procs dynamic array to 0
-	if (!nob_procs_flush(&procs)) return 1;
+	if (!nob_procs_flush(&procs)){
+		nob_log(NOB_ERROR, "Parallel source build failed");
+		return FAILED;
+	}
 
 	for (size_t i = 0; i < NOB_ARRAY_LEN(targets); ++i){
 		nob_cc_inputs(cmd, targets[i].bin_path);
 	}
-	return 0;
+	return SUCCESS;
 }
 
-int setup_web(Nob_Cmd *cmd){
-	if (!nob_mkdir_if_not_exists(WEB_FOLDER)) return 1;
-	if (nob_file_exists(WEB_FOLDER RESOURCES_FOLDER)){
-		if (!nob_delete_file(WEB_FOLDER RESOURCES_FOLDER)){
-			return 1;
-		}
-	}
-	if (!nob_mkdir_if_not_exists(WEB_FOLDER RESOURCES_FOLDER)) return 1;
-	if (!nob_copy_directory_recursively(RESOURCES_FOLDER, WEB_FOLDER RESOURCES_FOLDER)){
-		return 1;
-	}
-
-	return 0;
-}
 
 int main(int argc, char **argv){
 	NOB_GO_REBUILD_URSELF(argc, argv);
@@ -349,19 +295,19 @@ int main(int argc, char **argv){
 	while (argc > 0){
 		const char *command_name = nob_shift(argv, argc);
 		if (strcmp(command_name, "-debug") == 0){
-			is_debug = true;
+			current_config.is_debug = true;
 		}
 		else if (strcmp(command_name, "-name") == 0){
 			if (!(argc > 0)){
-				nob_log(NOB_ERROR, "[ERROR] No project name provided after `project`");
-				return 1;
+				nob_log(NOB_ERROR, "No project name provided after `project`");
+				return FAILED;
 			}
 			project_name = nob_shift(argv, argc);
 		}
 		else if (strcmp(command_name, "-platform") == 0){
 			if (!(argc > 0)){
-				nob_log(NOB_ERROR, "[ERROR] No project name provided after `project`");
-				return 1;
+				nob_log(NOB_ERROR, "No project name provided after `project`");
+				return FAILED;
 			}
 			const char *platform = nob_shift(argv, argc);
 			if (strcmp(platform, "web") == 0){
@@ -370,28 +316,27 @@ int main(int argc, char **argv){
 		}
 	}
 
-	if (!nob_mkdir_if_not_exists(DOWNLOAD_FOLDER)) return 1;
-	if (!nob_mkdir_if_not_exists(DEPENDENCY_FOLDER)) return 1;
-	if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
-	if (!nob_mkdir_if_not_exists(BIN_FOLDER)) return 1;
+	if (!nob_mkdir_if_not_exists(DOWNLOAD_FOLDER)) return FAILED;
+	if (!nob_mkdir_if_not_exists(DEPENDENCY_FOLDER)) return FAILED;
+	if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return FAILED;
+	if (!nob_mkdir_if_not_exists(BIN_FOLDER)) return FAILED;
 
 	struct SavedConfig saved_config = {0};
 	if (load_binary(&saved_config, sizeof(saved_config), BUILD_FOLDER CONFIG_FILE_NAME, config_version) == 0){
 		previous_config = &saved_config;
 	}
-
+	
 	Nob_Cmd cmd = {0};
-	if (setup_raylib(&cmd) != 0){
-		return 1;
+
+	if (current_config.platform == PLATFORM_WEB && setup_web(&cmd) == FAILED){
+		return FAILED;
 	}
 
-	if (current_config.platform == PLATFORM_WEB && setup_web(&cmd) != 0){
-		return 1;
-	}
+	if (setup_raylib(&cmd) == FAILED) return FAILED;
 
 	// Compile project
 	nob_cc(&cmd);
-	if (is_debug){
+	if (current_config.is_debug){
 		// Place inside build folder
 		nob_cc_output(&cmd, nob_temp_sprintf("%s%s", BUILD_FOLDER, project_name));
 	}
@@ -399,14 +344,14 @@ int main(int argc, char **argv){
 		// Place in root folder, next to resources folder
 		nob_cc_output(&cmd, project_name);
 	}
-	if (get_source_files(&cmd)) return 1;
+	if (get_source_files(&cmd)) return FAILED;
 	link_raylib(&cmd);
 
-	if (!nob_cmd_run(&cmd)) return 1;
+	if (!nob_cmd_run(&cmd)) return FAILED;
 
-	if (save_binary(&current_config, sizeof(current_config), BUILD_FOLDER CONFIG_FILE_NAME, config_version) != 0){
-		return 1;
+	if (save_binary(&current_config, sizeof(current_config), BUILD_FOLDER CONFIG_FILE_NAME, config_version) == FAILED){
+		return FAILED;
 	}
 
-	return 0;
+	return SUCCESS;
 }
