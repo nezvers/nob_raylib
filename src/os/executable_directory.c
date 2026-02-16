26fb9079bccea0fc78f584c827c688442c77dff8
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2026-02-16 22:59:33 +0200

    code formatting

--- src/os/executable_directory.c
+++ src/os/executable_directory.c
@@ -7,16 +7,17 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-int GetExecutableDirectory(char *buffer, size_t buffer_size)
-{
+int GetExecutableDirectory(char *buffer, size_t buffer_size) {
     DWORD len;
 
-    if (!buffer || buffer_size == 0)
+    if (!buffer || buffer_size == 0) {
         return -1;
+    }
 
     len = GetModuleFileNameA(NULL, buffer, (DWORD)buffer_size);
-    if (len == 0 || len == buffer_size)
+    if (len == 0 || len == buffer_size) {
         return -1;
+    }
 
     /* Strip filename */
     for (DWORD i = len; i > 0; --i) {
@@ -35,23 +36,26 @@ int GetExecutableDirectory(char *buffer, size_t buffer_size)
 #include <limits.h>
 #include <unistd.h>
 
-int GetExecutableDirectory(char *buffer, size_t buffer_size)
-{
+int GetExecutableDirectory(char *buffer, size_t buffer_size) {
     uint32_t size = (uint32_t)buffer_size;
 
-    if (!buffer || buffer_size == 0)
+    if (!buffer || buffer_size == 0) {
         return -1;
+    }
 
-    if (_NSGetExecutablePath(buffer, &size) != 0)
+    if (_NSGetExecutablePath(buffer, &size) != 0) {
         return -2; /* buffer too small */
+    }
 
     /* Resolve symlinks */
     char resolved[PATH_MAX];
-    if (!realpath(buffer, resolved))
+    if (!realpath(buffer, resolved)) {
         return -1;
+    }
 
-    if (strlen(resolved) >= buffer_size)
+    if (strlen(resolved) >= buffer_size) {
         return -2;
+    }
 
     strcpy(buffer, resolved);
 
@@ -71,19 +75,21 @@ int GetExecutableDirectory(char *buffer, size_t buffer_size)
 #include <unistd.h>
 #include <limits.h>
 
-int GetExecutableDirectory(char *buffer, size_t buffer_size)
-{
+int GetExecutableDirectory(char *buffer, size_t buffer_size) {
     ssize_t len;
 
-    if (!buffer || buffer_size == 0)
+    if (!buffer || buffer_size == 0) {
         return -1;
+    }
 
     len = readlink("/proc/self/exe", buffer, buffer_size - 1);
-    if (len == -1)
+    if (len == -1) {
         return -1;
+    }
 
-    if ((size_t)len >= buffer_size)
+    if ((size_t)len >= buffer_size) {
         return -2;
+    }
 
     buffer[len] = '\0';
 
@@ -100,8 +106,7 @@ int GetExecutableDirectory(char *buffer, size_t buffer_size)
 
 #else
 
-int GetExecutableDirectory(char *buffer, size_t buffer_size)
-{
+int GetExecutableDirectory(char *buffer, size_t buffer_size) {
     (void)buffer;
     (void)buffer_size;
     return -1; /* Unsupported platform */
